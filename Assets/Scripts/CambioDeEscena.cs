bc86210397ae4b3e35271a13f5bdf4f6b41a43f0
alexrodrd <a01667608@tec.mx>
2026-02-27 11:02:00 -0600

    Comprobacion de los botones al servidor

--- Assets/Scripts/CambioDeEscena.cs
+++ Assets/Scripts/CambioDeEscena.cs
@@ -1,22 +1,70 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using System.Collections;
+using System;
 
 public class CambioDeEscena : MonoBehaviour
-{
+{   
+    IEnumerator VerificarServidorYEjecutar(Action accionExitosa)
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost:8001/Home"))
+        {
+            www.timeout = 5; 
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("No se conectó al servidor: " + www.error);
+                
+            }
+            else
+            {
+                Debug.Log("Servidor disponible, cambiando de escena...");
+                accionExitosa.Invoke(); 
+            }
+        }
+    }
+
+    IEnumerator TalkToPython()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("user", "pablo");
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:8001/Home", form))
+        {
+            yield return www.SendWebRequest();
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                string txt = www.downloadHandler.text;
+                Debug.Log(txt);
+                SceneManager.LoadScene("Home"); 
+            }
+        }   
+    }
     public void IrAHome()
     {
-        SceneManager.LoadScene("Home"); 
+        StartCoroutine(TalkToPython());
     }
     public void IrAInicioSesion()
     {
-        SceneManager.LoadScene("Inicio sesion");
+        StartCoroutine(VerificarServidorYEjecutar(() => {
+            SceneManager.LoadScene("Inicio sesion");
+        }));
     }
     public void IrARegistro()
     {
-        SceneManager.LoadScene("Registro");
+        StartCoroutine(VerificarServidorYEjecutar(() => {
+            SceneManager.LoadScene("Registro");
+        }));
     }
     public void IrAMenu()
     {
-        SceneManager.LoadScene("Menu");
+        StartCoroutine(VerificarServidorYEjecutar(() => {
+            SceneManager.LoadScene("Menu");
+        }));
     }
 }
